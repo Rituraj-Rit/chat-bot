9671b809c643c80f382c2595a43d4c6a1c98554b
Rituraj Verma <riturajverma340@gmail.com>
2026-07-02 13:38:35 +0530

    originupdate

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { 
   cors:{
     // origin:"http://localhost:5173",
-    origin:"https://chat-bot-opal-tau.vercel.app/",
+    origin:"https://chat-bot-opal-tau.vercel.app",
   }
  });
 
